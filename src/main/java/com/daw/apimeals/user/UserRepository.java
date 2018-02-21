d5414881d1f4c5faa4f024497fcaa4ad8b6c007e
juanAntonMarazuela <je.anton@alumnos.urjc.es>
2018-02-21 23:21:46 +0100

    Changes to Repository

--- src/main/java/com/daw/apimeals/user/UserRepository.java
+++ src/main/java/com/daw/apimeals/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.daw.apimeals.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
 
 }
