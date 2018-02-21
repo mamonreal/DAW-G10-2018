6a8d553a4ef0b8ae1793a31737990e81ba49ec64
juanAntonMarazuela <je.anton@alumnos.urjc.es>
2018-02-21 23:11:46 +0100

    Changes to Entity user

--- src/main/java/com/daw/apimeals/user/User.java
+++ src/main/java/com/daw/apimeals/user/User.java
@@ -25,6 +25,81 @@ public class User {
 	
 	
 	@ElementCollection(fetch = FetchType.EAGER)
-	 private List<String> roles;
+	private List<String> roles;
+	
+	protected User() {};
+	
+	public User(String name,String userName,String email,String passwordHash,String addres,String telephone) {
+		this.name=name;
+		this.userName=userName;
+		this.email=email;
+		this.passwordHash=passwordHash;
+		this.addres=addres;
+	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getAddres() {
+		return addres;
+	}
+
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+
+	public long getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(long telephone) {
+		this.telephone = telephone;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	
+	
 }
