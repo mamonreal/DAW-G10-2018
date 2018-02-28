3689c40f349cc860cd8622dc063751b852da8f92
Miguel Ángel Monreal Velasco <ma.monreal@alumnos.urjc.es>
2018-02-28 11:48:56 +0100

    Show the orders of a registered user

--- src/main/java/com/daw/apimeals/order/OrderController.java
+++ src/main/java/com/daw/apimeals/order/OrderController.java
@@ -1,14 +1,30 @@
 package com.daw.apimeals.order;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.daw.apimeals.menu.MenuRepository;
+import com.daw.apimeals.user.User;
+import com.daw.apimeals.user.UserComponent;
 
 @Controller
 public class OrderController {
 
 	@Autowired 
 	private OrderRepository oRepository;
+	
+	@RequestMapping("/order")
+	public String order(Model model) {
+		
+		List<Order> orders = new UserComponent().getLoggedUser().getOrders();
+		
+		model.addAttribute("orders", orders);
+		
+		return "order";
+	}
 }
 
--- src/main/java/com/daw/apimeals/order/OrderRepository.java
+++ src/main/java/com/daw/apimeals/order/OrderRepository.java
@@ -2,6 +2,6 @@ package com.daw.apimeals.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long> {
 	
 }
