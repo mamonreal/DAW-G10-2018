ebdfcae08d3de21181e098869629e2a404cfc293
Miguel Ángel Monreal Velasco <ma.monreal@alumnos.urjc.es>
2018-02-28 16:53:41 +0100

    Init database with an user

--- src/main/java/com/daw/apimeals/user/UserController.java
+++ src/main/java/com/daw/apimeals/user/UserController.java
@@ -1,16 +1,15 @@
 package com.daw.apimeals.user;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.daw.apimeals.menu.MenuRepository;
 import com.daw.apimeals.service.MainService;
 
 @Controller
@@ -21,6 +20,12 @@ public class UserController extends MainService {
 	
 	@Autowired
 	private UserComponent userComponent;
+	
+	@PostConstruct
+	public void init() {
+		uRepository.save(new User("Jorge", "689542138", "jorge@gmail.com",
+				"theyorch", "paco", "Manchester", "C/ Ave del paraiso", "28913"));
+	}
 	
 	@RequestMapping("/user")
 	public String user(Model model) {
