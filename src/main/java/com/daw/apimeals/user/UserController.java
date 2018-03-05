03c6a35e40fa65063e3377c50f3a8b833bda67a1
mamonreal <mamonreal@localhost.localdomain>
2018-03-05 17:31:41 +0100

    Revert "Fixed the error in the log in already"
    This reverts commit 12d373a4452eadd904acf3cce277517d5ef0969e.

--- src/main/java/com/daw/apimeals/user/UserController.java
+++ src/main/java/com/daw/apimeals/user/UserController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -28,7 +27,7 @@ public class UserController extends MainService {
 		uRepository.save(new User("jorge", "647585912", "j.gamero@gmailcom","theyorch11", "pass", "city", "address", "PC", "ROLE_ADMIN","ROLE_USER"));
 	}
 	
-	@GetMapping("/user")
+	@RequestMapping("/user")
 	public String user(Model model) {
         Boolean b = userComponent.isLoggedUser();
         model.addAttribute("logged", b);
--- src/main/java/com/daw/apimeals/user/UserRepository.java
+++ src/main/java/com/daw/apimeals/user/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends  CrudRepository<User, Long>  {
 	
 	User findByName(String name);
-	//User findByUserName(String UserName);
 
 
 }
