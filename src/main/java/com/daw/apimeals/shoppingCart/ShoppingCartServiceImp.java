e35b07b9d3bea792a87ce1c529ec08655a548d99
Almendron100 <David.a.almendra@hotmail.com>
2018-03-06 02:05:16 +0100

    shoppingCart package, changed variable atributes in few classes

---
+++ src/main/java/com/daw/apimeals/shoppingCart/ShoppingCartServiceImp.java
@@ -0,0 +1,74 @@
+package com.daw.apimeals.shoppingCart;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.daw.apimeals.product.ProductRepository;
+import com.daw.apimeals.product.Product;
+
+@Service
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Transactional
+public class ShoppingCartServiceImp implements ShoppingCartService {
+	
+	@Autowired
+	private ProductRepository pRepository;
+	
+	private Map<Product, Integer> products = new HashMap<>();
+
+	@Override
+	public void addProduct(Product product) {
+		if (products.containsKey(product)) {
+			products.replace(product, products.get(product)+1);
+		}
+		else {
+			products.put(product, 1);
+		}
+		
+	}
+
+	@Override
+	public void removeProduct(Product product) {
+		if (products.containsKey(product)) {
+            if (products.get(product) > 1)
+                products.replace(product, products.get(product) - 1);
+            else if (products.get(product) == 1) {
+                products.remove(product);
+            }
+        }
+		
+	}
+
+	@Override
+	public Map<Product, Integer> getProductsInShoppingCart() {
+		return Collections.unmodifiableMap(products);
+	}
+
+	@Override
+	public void checkout() {
+		Product product;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            // Refresh quantity for every product before checking
+            product = pRepository.findOne(entry.getKey().getId());
+        }
+        pRepository.save(products.keySet());
+        pRepository.flush();
+        products.clear();
+		
+	}
+
+	@Override
+	public Long getTotal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
