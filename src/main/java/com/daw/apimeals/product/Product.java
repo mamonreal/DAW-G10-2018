6e01466e3fe9741bc1ef4780a221e7a1c8bf5372
mamonreal <ma.monreal@alumnos.urjc.es>
2018-03-08 01:23:09 +0100

    Implementing shopping cart

--- src/main/java/com/daw/apimeals/product/Product.java
+++ src/main/java/com/daw/apimeals/product/Product.java
@@ -117,6 +117,28 @@ public class Product{
 	public void setPath(String path) {
 		this.path = path;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	
 	
 	
