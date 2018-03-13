dc319bef2faeb252020fa28fc3477b594a3de1f1
jorge g <jorgegb_13@hotmail.com>
2018-03-13 09:49:10 +0100

    try to do the email funcionality

--- src/main/java/com/daw/apimeals/service/EmailService.java
+++ src/main/java/com/daw/apimeals/service/EmailService.java
@@ -36,15 +36,15 @@ public class EmailService {
 		MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
 				StandardCharsets.UTF_8.name());
 
-		Template t = freemarkerConfig.getTemplate("emailTemplate.ftl");
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("name", user.getName()); 
-		String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
-		helper.setTo(user.getEmail());
-		helper.setText(html, true);
-		helper.setSubject("Gracias por registrarte en Apimeals");
+		//Template t = freemarkerConfig.getTemplate("emailTemplate.ftl");
+		//Map<String, Object> model = new HashMap<String, Object>();
+		//model.put("name", user.getName()); 
+		//String html = FreeMarkerTemplateUtils.processTemplateIntoString(t, model);
+		//helper.setTo(user.getEmail());
+		//helper.setText(html, true);
+		//helper.setSubject("Gracias por registrarte en Apimeals");
 
-		emailSender.send(message);
+		//emailSender.send(message);
 	}
 
-}*/
+}*/ 
