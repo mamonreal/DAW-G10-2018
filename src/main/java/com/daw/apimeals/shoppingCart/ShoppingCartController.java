5655c2609f6b813514cb21a0895a61f3798b7b5c
mamonreal <ma.monreal@alumnos.urjc.es>
2018-03-21 16:24:59 +0100

    Remove products from shopping cart

--- src/main/java/com/daw/apimeals/shoppingCart/ShoppingCartController.java
+++ src/main/java/com/daw/apimeals/shoppingCart/ShoppingCartController.java
@@ -7,11 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.daw.apimeals.product.ProductRepository;
@@ -54,7 +50,7 @@ public class ShoppingCartController extends MainService{
 		return "shoppingCart";
     }
 
-    @GetMapping("/shoppingCart/removeProduct/{id}")
+    @PostMapping("/shoppingCart/removeProduct/{id}")
     public String removeProductFromCart(@PathVariable("id") Long id) {
         shoppingCartService.removeProduct(productRepository.findById(id));
         return "shoppingCart";
