78f18ae69e5d41cc49da7a1d1c8b18525b8efc77
juanAntonMarazuela <je.anton@alumnos.urjc.es>
2018-05-28 13:29:19 +0200

    Add menus

--- src/main/java/com/daw/apimeals/menu/MenuController.java
+++ src/main/java/com/daw/apimeals/menu/MenuController.java
@@ -26,14 +26,24 @@ public class MenuController {
 	
 	@PostConstruct
 	public void init() {
-		Menu m1,m2,m3;
+		Menu m1,m2,m3,m4,m5,m6,m7,m8;
 		m1=new Menu(1,"Veggie","Vegano","Menu vegano","lunch","306","",12);
 		m2=new Menu(2,"Mediterranean","Mediterraneo","Desayuno mediterraneo","breakfast","44","",12);
 		m3=new Menu(3,"American","Americano","Cena americana","dinner","306","",12);
+		m4=new Menu(4,"Sushi","Japones","Bandeja de 12 raciones","dinner","243","",15);
+		m5=new Menu(5,"Mexican","Mejicano","Fajita,burrito y nachos","lunch","320","",8);
+		m6=new Menu(6,"Menú valenciano","Espanol","Paella valenciana mas entrantes","lunch","356","",13);
+		m7=new Menu(7,"Italian","Italiano","Pizza italiana y pizza","dinner","360","",7);
+		m8=new Menu(8,"British","Britanico","Tortitas,bacon y huevo","breakfast","300","",9);
 		
 		mRepository.save(m1);
 		mRepository.save(m2);
 		mRepository.save(m3);
+		mRepository.save(m4);
+		mRepository.save(m5);
+		mRepository.save(m6);
+		mRepository.save(m7);
+		mRepository.save(m8);
 	}
 	
 	
