7c8722c8252547754d28b4ea0bf095dbaf952b66
Almendron100 <David.a.almendra@hotmail.com>
2018-06-01 22:24:07 +0200

    Starting ApiRest

--- src/main/java/com/daw/apimeals/user/UserRepository.java
+++ src/main/java/com/daw/apimeals/user/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends  CrudRepository<User, Long>  {
 	
 	User findByName(String name);
+	User getById(long id);
 
 
 }
