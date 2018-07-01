7ca136940e202805e260cfb154a6cad2be85fb78
David Amor <David.a.almendra@hotmail.com>
2018-07-01 17:31:54 +0200

    Product html and user html

--- AngularApimeals/src/app/Product/product.component.ts
+++ AngularApimeals/src/app/Product/product.component.ts
@@ -5,6 +5,8 @@ import { ProductService} from './product.service';
 import {Http} from '@angular/http';
 import {LoginService} from '../login/login.service';
 import { environment } from '../../environments/environment';
+import { Cart } from '../Interfaces/Cart/shoppingcart.model';
+import { CartService } from '../Cart/cart.service';
 
 
 @Component({
@@ -23,8 +25,10 @@ export class ProductComponent implements OnInit {
     kc:string|number;
     path?:string;
     price:number;*/
+    private carts: Cart[];
+    private cart: Cart;
 
-    constructor(private router: Router, activatedRoute: ActivatedRoute, public productService: ProductService){
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, public productService: ProductService, private cService: CartService){
         this.id = activatedRoute.snapshot.params['id'];
 
 
@@ -40,5 +44,24 @@ export class ProductComponent implements OnInit {
 
     getProduct(id:number){
         
+    }
+
+    addToCart(){
+    
+        this.cService.createCart(this.activatedRoute.snapshot.params['id']).subscribe(
+          response => {  this.router.navigate(['/cart']);},
+          error => console.log(error) 
+        );
+      }
+
+    updateCart(){
+
+    for (let cart of this.carts){
+    this.cService.getUCart(cart.id);
+    this.cService.addProductToCart(cart, this.activatedRoute.snapshot.params['id']).subscribe(
+        response => {  this.router.navigate(['/shoppingCart']);},
+        error => console.log(error) 
+        );
+      }
     }
 }
